a29c6e3e20bc21cafe2151c11bee900926cbc935
Aa7009975 <aa7009975@gmail.com>
2024-11-06 08:35:27 +0530

    Update sum_digits.py

--- 04.10.2024/sum_digits.py
+++ 04.10.2024/sum_digits.py
@@ -1,1 +1,8 @@
+def sum_digits(num):
+    num = abs(num)
+    return sum(int(digit) for digit in str(num))
+
+user_input = int(input("Enter an integer: "))
+
+print("The sum of the digits is:", sum_digits(user_input))
 
